6c10ea44d5bc8889006d96bd707f67cc0c2c9920
leyla sabouri <dev.leyla.sabouri@gmail.com>
2021-10-30 23:14:40 +0330

    add layout page in blade

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="rtl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>مدیریت منو</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <div class="card card-header">
+                    مدیریت منو
+                </div>
+                <div class="card-body">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
